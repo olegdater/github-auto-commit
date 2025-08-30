f868cbd290aceecac6c09e441c5a3f1f4e4a5331
Oleg Nederev <olegwn@gmail.com>
2025-08-30 10:33:26 +0300

    Add requirement to remove whitespace

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ async function autoCommit() {
 
     const prompt = `I am working on a software project using github.
     I want to summarize the following input into a git commit message, can be multiple lines.
-    that is no longer than 6 words or less in each line. 
+    that is no longer than 6 words or less in each line, do not add white space between lines. 
     No need to add any other text, just plain straight commit message.
     Please summarize the following git diff in a very short, concise message 
     that I can use as a commit message. 
